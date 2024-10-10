25b8491d5692cbb1ee1d705e9f2bc59359db3b0c
Nicolas <nicolas.marsil02@gmail.com>
2024-10-10 17:23:09 -0300

    feat: starting project

---
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/GamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.nmsilos.gamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesApiApplication.class, args);
+    }
+
+}
---
+++ games-api/src/test/java/com/github/nmsilos/gamesapi/GamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nmsilos.gamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
