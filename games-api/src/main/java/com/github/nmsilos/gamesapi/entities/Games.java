5c04288e8be0b0039fcb564215c78ebed2e71036
Nicolas <nicolas.marsil02@gmail.com>
2024-10-10 18:43:34 -0300

    feat: created entities Games and Platform

---
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/entities/Games.java
@@ -0,0 +1,47 @@
+package com.github.nmsilos.gamesapi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.Year;
+import java.util.*;
+
+@Entity
+@Table(name = "games")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Games implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gameId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "developerCompany", nullable = false)
+    private String developerCompany;
+
+    @Column(name = "releaseYear")
+    private Year releaseYear;
+
+    @ManyToMany
+    @JoinTable(
+            name = "games_platforms",
+            joinColumns = @JoinColumn(name = "gameId"),
+            inverseJoinColumns = @JoinColumn(name = "platformId"))
+    private List<Platform> platforms = new ArrayList<>();
+
+    public Games(String title, String description, String developerCompany, Year releaseYear) {
+        this.title = title;
+        this.description = description;
+        this.developerCompany = developerCompany;
+        this.releaseYear = releaseYear;
+    }
+}
---
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/entities/Platform.java
@@ -0,0 +1,32 @@
+package com.github.nmsilos.gamesapi.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "platforms")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Platform implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long platformId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "platforms")
+    private List<Games> games = new ArrayList<Games>();
+
+    public Platform(String name) {
+        this.name = name;
+    }
+}
