a129ec1368a09839c307ed58ca4aecd6b5de9563
Nicolas <nicolas.marsil02@gmail.com>
2024-10-11 19:42:00 -0300

    refactor: method create in GameController

--- games-api/src/main/java/com/github/nmsilos/gamesapi/controller/GameController.java
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/controller/GameController.java
@@ -1,9 +1,13 @@
 package com.github.nmsilos.gamesapi.controller;
 
+import com.github.nmsilos.gamesapi.dto.GameCreateDTO;
+import com.github.nmsilos.gamesapi.dto.GameResponseDTO;
 import com.github.nmsilos.gamesapi.entity.Game;
 import com.github.nmsilos.gamesapi.entity.Platform;
+import com.github.nmsilos.gamesapi.mapper.GameMapper;
 import com.github.nmsilos.gamesapi.service.GameService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +21,10 @@ public class GameController {
     private final GameService gameService;
 
     @PostMapping
-    public ResponseEntity<Game> create(@RequestBody Game game) {
-        gameService.create(game);
-        return ResponseEntity.ok().body(game);
+    public ResponseEntity<GameResponseDTO> create(@RequestBody GameCreateDTO gameDto) {
+        Game game = GameMapper.toGame(gameDto);
+        GameResponseDTO response = GameMapper.toGameResponseDto(gameService.create(game));
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/{id}")
