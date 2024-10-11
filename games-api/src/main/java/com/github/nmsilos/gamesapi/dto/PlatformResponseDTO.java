31f72262f15d3cc20c7a6fb4e0c7524df2562634
Nicolas <nicolas.marsil02@gmail.com>
2024-10-11 19:34:51 -0300

    feat: created ResponseDTOs for Game and Platform

---
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/dto/PlatformResponseDTO.java
@@ -0,0 +1,20 @@
+package com.github.nmsilos.gamesapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlatformResponseDTO {
+
+    private Long id;
+    private String name;
+    private Set<GameResponseDTO> games;
+
+}
