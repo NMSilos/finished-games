222529e9a5333ce5b18082b61886f2e91ff3cf15
Nicolas <nicolas.marsil02@gmail.com>
2024-10-11 00:31:50 -0300

    feat: created GameService

---
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/service/GameService.java
@@ -0,0 +1,16 @@
+package com.github.nmsilos.gamesapi.service;
+
+import com.github.nmsilos.gamesapi.entity.Game;
+import com.github.nmsilos.gamesapi.repository.GameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GameService {
+
+    private final GameRepository gameRepository;
+
+    
+}
