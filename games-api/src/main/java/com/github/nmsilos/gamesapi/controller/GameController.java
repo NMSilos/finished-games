dcb502be7ac22d1fa827f81cf87d86dfe0db1e9a
Nicolas <nicolas.marsil02@gmail.com>
2024-10-11 00:30:31 -0300

    feat: created GameController

---
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/controller/GameController.java
@@ -0,0 +1,19 @@
+package com.github.nmsilos.gamesapi.controller;
+
+import com.github.nmsilos.gamesapi.entity.Game;
+import com.github.nmsilos.gamesapi.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/games")
+public class GameController {
+
+    private final GameService gameService;
+
+}
