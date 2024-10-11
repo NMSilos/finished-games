704f78a225f863fdfae58ee828230b2ce4bef063
Nicolas <nicolas.marsil02@gmail.com>
2024-10-10 23:41:25 -0300

    feat: method getAll in PlatformController and PlatformService

--- games-api/src/main/java/com/github/nmsilos/gamesapi/controller/PlatformController.java
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/controller/PlatformController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/platforms")
@@ -17,6 +19,12 @@ public class PlatformController {
     public ResponseEntity<Platform> create(@RequestBody Platform platform) {
         platformService.create(platform);
         return ResponseEntity.ok().body(platform);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Platform>> getAll() {
+        List<Platform> platforms = platformService.getAll();
+        return ResponseEntity.ok().body(platforms);
     }
 
 }
--- games-api/src/main/java/com/github/nmsilos/gamesapi/service/PlatformService.java
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/service/PlatformService.java
@@ -19,5 +19,8 @@ public class PlatformService {
         return platformRepository.save(platform);
     }
 
-    
+    @Transactional(readOnly = true)
+    public List<Platform> getAll() {
+        return platformRepository.findAll();
+    }
 }
