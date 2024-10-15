6c44548004079ce1f0991b57be55ce0c6d6c6a36
Nicolas <nicolas.marsil02@gmail.com>
2024-10-15 17:49:54 -0300

    feat: created methods for mapping noListDTO

--- games-api/src/main/java/com/github/nmsilos/gamesapi/mapper/GameMapper.java
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/mapper/GameMapper.java
@@ -1,11 +1,14 @@
 package com.github.nmsilos.gamesapi.mapper;
 
 import com.github.nmsilos.gamesapi.dto.GameCreateDTO;
+import com.github.nmsilos.gamesapi.dto.GameNoListDTO;
 import com.github.nmsilos.gamesapi.dto.GameResponseDTO;
 import com.github.nmsilos.gamesapi.entity.Game;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GameMapper {
@@ -16,6 +19,14 @@ public class GameMapper {
 
     public static Game toGame(GameCreateDTO dto) {
         return new ModelMapper().map(dto, Game.class);
+    }
+
+    public static GameNoListDTO toGetAllNoListDto(Game game) {
+        return new ModelMapper().map(game, GameNoListDTO.class);
+    }
+
+    public static List<GameNoListDTO> toGetAllGameResponseDto(List<Game> games) {
+        return games.stream().map(GameMapper::toGetAllNoListDto).toList();
     }
 
 }
--- games-api/src/main/java/com/github/nmsilos/gamesapi/mapper/PlatformMapper.java
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/mapper/PlatformMapper.java
@@ -1,21 +1,30 @@
 package com.github.nmsilos.gamesapi.mapper;
 
-import com.github.nmsilos.gamesapi.dto.PlatformCreateDTO;
-import com.github.nmsilos.gamesapi.dto.PlatformResponseDTO;
+import com.github.nmsilos.gamesapi.dto.*;
 import com.github.nmsilos.gamesapi.entity.Platform;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PlatformMapper {
 
-    public static PlatformResponseDTO toPlatformResponseDTO(Platform platform) {
+    public static PlatformResponseDTO toPlatformResponseDto(Platform platform) {
         return new ModelMapper().map(platform, PlatformResponseDTO.class);
     }
 
     public static Platform toPlatform(PlatformCreateDTO dto) {
         return new ModelMapper().map(dto, Platform.class);
+    }
+
+    public static PlatformNoListDTO toGetAllNoListDto(Platform platform) {
+        return new ModelMapper().map(platform, PlatformNoListDTO.class);
+    }
+
+    public static List<PlatformNoListDTO> toGetAllPlatformsResponseDto(List<Platform> platforms) {
+        return platforms.stream().map(PlatformMapper::toGetAllNoListDto).toList();
     }
 
 }
