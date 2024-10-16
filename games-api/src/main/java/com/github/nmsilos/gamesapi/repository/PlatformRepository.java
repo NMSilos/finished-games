8943d359d39c31b8277465876d0de8beb08fd190
Nicolas <nicolas.marsil02@gmail.com>
2024-10-16 17:33:37 -0300

    feat: created existsByName in PlatformRepository

--- games-api/src/main/java/com/github/nmsilos/gamesapi/repository/PlatformRepository.java
+++ games-api/src/main/java/com/github/nmsilos/gamesapi/repository/PlatformRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlatformRepository extends JpaRepository<Platform, Long> {
+
+    boolean existsByName(String name);
+
 }
