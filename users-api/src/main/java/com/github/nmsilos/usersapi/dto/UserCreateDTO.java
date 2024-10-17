51fb8cf4bda4b384fee0e2c6d76a227ebf5b68ed
Nicolas <nicolas.marsil02@gmail.com>
2024-10-17 11:03:20 -0300

    feat: created UserCreateDTO

---
+++ users-api/src/main/java/com/github/nmsilos/usersapi/dto/UserCreateDTO.java
@@ -0,0 +1,32 @@
+package com.github.nmsilos.usersapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCreateDTO {
+
+    @NotNull
+    @Size(min = 6, max = 20)
+    private String username;
+
+    @NotNull
+    @Size(min = 8)
+    private String password;
+
+    @NotNull
+    @Size(max = 50)
+    private String name;
+
+    @Email
+    private String email;
+
+}
