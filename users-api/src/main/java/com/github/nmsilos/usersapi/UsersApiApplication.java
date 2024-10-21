cb14850f9d38da65bb868be7625666dd827719d5
Nicolas <nicolas.marsil02@gmail.com>
2024-10-21 14:38:52 -0300

    feat: feign client configs

--- users-api/src/main/java/com/github/nmsilos/usersapi/UsersApiApplication.java
+++ users-api/src/main/java/com/github/nmsilos/usersapi/UsersApiApplication.java
@@ -2,8 +2,10 @@ package com.github.nmsilos.usersapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UsersApiApplication {
 
     public static void main(String[] args) {
