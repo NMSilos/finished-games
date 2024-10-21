8c4817902eda1062b7ae49354a3ab8637a7b34e8
Nicolas <nicolas.marsil02@gmail.com>
2024-10-21 15:32:12 -0300

    feat: created exception classes

---
+++ users-api/src/main/java/com/github/nmsilos/usersapi/exception/ApiExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.github.nmsilos.usersapi.exception;
+
+import com.github.nmsilos.usersapi.exception.custom.DefaultErrorMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DefaultErrorMessage>handleInvalidArgumentException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DefaultErrorMessage(request, HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+
+}
---
+++ users-api/src/main/java/com/github/nmsilos/usersapi/exception/custom/InvalidArgumentException.java
@@ -0,0 +1,8 @@
+package com.github.nmsilos.usersapi.exception.custom;
+
+public class InvalidArgumentException extends RuntimeException {
+    public InvalidArgumentException(String msg) {
+        super(msg);
+    }
+
+}
