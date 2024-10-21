140218b380a9fae2d0900438bfd11d79b834e42d
Nicolas <nicolas.marsil02@gmail.com>
2024-10-21 15:44:17 -0300

    feat: handling DataIntegrityViolationException

--- users-api/src/main/java/com/github/nmsilos/usersapi/exception/ApiExceptionHandler.java
+++ users-api/src/main/java/com/github/nmsilos/usersapi/exception/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.github.nmsilos.usersapi.exception;
 import com.github.nmsilos.usersapi.exception.custom.DefaultErrorMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,6 +17,11 @@ public class ApiExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<DefaultErrorMessage>handleInvalidArgumentException(MethodArgumentNotValidException ex, HttpServletRequest request) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DefaultErrorMessage(request, HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<DefaultErrorMessage>handleDataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new DefaultErrorMessage(request, HttpStatus.CONFLICT, ex.getMessage()));
     }
 
 
